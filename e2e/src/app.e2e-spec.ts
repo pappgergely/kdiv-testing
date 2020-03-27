b13e2ff3f24558ae2272b7373fa181e5a56ea4c7
Papp Gergely <papp.gergely@aurettoworks.com>
2020-03-27 11:30:48 +0100

    written some e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+import {Gender, Student} from "../../src/app/interfaces/student"; 
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,10 +9,31 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should navigate to add screen', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('kdiv-testing app is running!');
+    page.clickHeaderButton();
+    expect(page.getHeaderText()).toBe('Hozzáadás');
+    expect(page.getHeaderButtonText()).toBe('Vissza');
   });
+
+  it('should add a new student', () => {
+    const student: Student = {
+      name: 'Gipsz Jakab ' + (new Date().getTime()),
+      age: 42,
+      email: 'gipsz.jakab@kdiv.hu',
+      gender: Gender.MALE,
+    };
+    page.navigateTo('add-student');
+    page.fillStudentForm(student);
+    page.clickAddButton();
+    expect(page.getUrl()).toBe(browser.baseUrl + 'students');
+    expect(page.isStudentTableRowContaining(student.name)).toBeTruthy();
+  });
+
+  // sikeres módosítás
+  // rossz email cím esetén hibaüzenet a módosításkor, nem klikkelhető submit gomb
+  // módosításnál többi hibaüzenetet is végigellenőrizni
+  // törlés
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,46 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
+import {Gender, Student} from "../../src/app/interfaces/student"; 
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  navigateTo(url?: string): Promise<unknown> {
+    return browser.get(browser.baseUrl + (url ? url : '')) as Promise<unknown>;
   }
-
+  getUrl(): Promise<string> {
+    return browser.getCurrentUrl() as Promise<string>;
+  }
+  /*
   getTitleText(): Promise<string> {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
+  }
+  */
+
+  private getHeaderButton(): ElementFinder {
+    return element(by.css('app-header .btn'));
+  }
+  clickHeaderButton(): Promise<void> {
+    return this.getHeaderButton().click() as Promise<void>;
+  }
+  getHeaderText(): Promise<string> {
+    return element(by.css('app-header h1')).getText() as Promise<string>;
+  }
+  getHeaderButtonText(): Promise<string> {
+    return this.getHeaderButton().getText() as Promise<string>;
+  }
+
+  private getInput(id: string): ElementFinder {
+    return element(by.id(id));
+  }
+  fillStudentForm(student: Student): Promise<void> {
+    this.getInput('inputName').sendKeys(student.name);
+    this.getInput('inputEmail').sendKeys(student.email);
+    this.getInput('inputAge').sendKeys(student.age);
+    return this.getInput('inputGender').sendKeys(student.gender === Gender.MALE ? 'Férfi' : 'Nő') as Promise<void>;
+  }
+  clickAddButton(): Promise<void> {
+    return element(by.buttonText('Mentés')).click() as Promise<void>;
+  }
+
+  isStudentTableRowContaining(s: string): Promise<boolean> {
+    return element(by.cssContainingText('tbody tr', s)).isPresent() as Promise<boolean>;
   }
 }
